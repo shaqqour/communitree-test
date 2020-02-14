434c607422338e5095135125717ea2ba44e9cd64
Shaqqour <mshaqqourm@gmail.com>
2020-02-14 10:03:17 -0800

    Add table relations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+    has_many :services
+    has_many :users, through: :services
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  belongs_to :location
+  has_many :services
+  has_many :categories, through: :services
+
 end
