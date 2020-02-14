38b9da10a304e6309ada81563a24c1722ea64593
Shaqqour <mshaqqourm@gmail.com>
2020-02-14 09:10:57 -0800

    Create service

---
+++ db/migrate/20200214171005_create_services.rb
@@ -0,0 +1,13 @@
+class CreateServices < ActiveRecord::Migration[6.0]
+  def change
+    create_table :services do |t|
+      t.string :title
+      t.string :description
+      t.string :ask_offer
+      t.integer :user_id
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
