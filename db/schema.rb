1810da3d9b226463b6b401e56a96a688d3154d4d
Shaqqour <mshaqqourm@gmail.com>
2020-02-14 09:06:51 -0800

    Create category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_235410) do
+ActiveRecord::Schema.define(version: 2020_02_14_170207) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
