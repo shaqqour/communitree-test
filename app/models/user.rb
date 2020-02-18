373b448088ecec7b1cc0c86af19dc3b4abef88aa
Shaqqour <mshaqqourm@gmail.com>
2020-02-18 14:20:36 -0800

    Login via facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
 
   belongs_to :location
   has_many :services
@@ -10,6 +10,20 @@ class User < ApplicationRecord
 
   def location_attributes=(location_attributes)
     self.location = Location.find_or_create_by(location_attributes)
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.username = user.email
+      user.password = "123456"
+      user.location = Location.find_or_create_by(city: "U/A", state: "U/A", country: "U/A")
+      #user.name = auth.info.name   # assuming the user model has a name
+      #user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
   end
 
 end
