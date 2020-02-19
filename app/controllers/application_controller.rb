47eb3008de14bfe11917f0f5ccf6d56a54e7bac0
Shaqqour <mshaqqourm@gmail.com>
2020-02-19 11:05:14 -0800

    Fix update location bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
     protected
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username, location_attributes: [:city, :state, :country]])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:username, location_attributes: [:city, :state, :country]])
     end
 
 end
